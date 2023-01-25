bf110e6850840679afb7bd17f86ab7ef0c80c9b3
Sai Saing Linn <saisaing.linn@kbzbank.com>
2023-01-25 16:49:58 +0630

    null exception in detail js

--- src/components/detail-info/detail-info.js
+++ src/components/detail-info/detail-info.js
@@ -1,9 +1,11 @@
 // click on #map-icon and scroll to #map and made offset top 30px
-let mapIcon = document.querySelector('#map-icon');
-mapIcon.addEventListener('click', function (e) {
-  e.preventDefault();
-  window.scrollTo({
-    top: document.querySelector('#map').offsetTop - 30,
-    behavior: 'smooth'
+let mapIcon = document.querySelector('.detail-info-wrapper #map-icon');
+if (mapIcon) {
+  mapIcon.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: document.querySelector('.detail-info-wrapper #map').offsetTop - 30,
+      behavior: 'smooth'
+    });
   });
-});
+}
